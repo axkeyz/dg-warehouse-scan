ee854740173c7fad50d29d420dc243062ff38461
AH work lap <aileen@divers.co.nz>
2021-04-15 11:38:48 +1200

    added bulk transfer

--- public/queries.php
+++ public/queries.php
@@ -10,7 +10,7 @@ if( APP_DEBUG ){
 }
 
 $location = $_REQUEST['location'];
-$item = $_REQUEST['item'] ?? '';
+$item = $_REQUEST['item'] ? explode(",", $_REQUEST['item']) : null;
 $save = $_REQUEST['save'] ?? null;
 $get_errors = [];
 
@@ -33,40 +33,49 @@ if(isset($location)){
 }
 
 if(isset($item)){
-    if(strlen($item) >= 7){
-        // Check for existing OriginalItemCode (the one that gets scanned)
-        $details = (new Query("Select OriginalItemCode, ItemCode from StockItem where OriginalItemCode = ?", [$item]))->get_results();
+    $new_items_details = [];
+    $new_items_details_checks = [];
+    $saved = 'table-warning';
+    foreach($item as $i){
+        if(strlen($i) >= 7){
+            // Check for existing OriginalItemCode (the one that gets scanned)
+            $details = (new Query("Select OriginalItemCode, ItemCode from StockItem where OriginalItemCode = ?", [$i]))->get_results();
 
-        if(is_array($details)){
-            $new_item_details = (new Query("Select OriginalItemCode as 'Scanned Item Code', ItemCode as 'Current Item Code', WarehouseLocation.Name as Location, Status.Name as 'Shipping Status' 
-                            From StockItem left join WarehouseLocation on StockItem.Location = WarehouseLocation.Code left join Status on StockItem.Status = Status.Code Where OriginalItemCode = ?", [$item]))->get_results()[0];
-        }else{
-            $new_item_details = (new Query("Select OriginalItemCode as 'Scanned Item Code', ItemCode as 'Current Item Code', WarehouseLocation.Name as Location, Status.Name as 'Shipping Status' 
-            From StockItem left join WarehouseLocation on StockItem.Location = WarehouseLocation.Code left join Status on StockItem.Status = Status.Code Where ItemCode = ?", [$item]))->get_results();
-
-            if($new_item_details == null){
-                $get_errors[] = "Item Code";
+            if(is_array($details)){
+                $new_item_details = (new Query("Select OriginalItemCode as 'Scanned Item Code', ItemCode as 'Current Item Code', WarehouseLocation.Name as Location, Status.Name as 'Shipping Status' 
+                                From StockItem left join WarehouseLocation on StockItem.Location = WarehouseLocation.Code left join Status on StockItem.Status = Status.Code Where OriginalItemCode = ?", [$i]))->get_results()[0];
+                                $new_items_details[] = $new_item_details;
             }else{
-                $new_item_details = $new_item_details[0];
+                $new_item_details = (new Query("Select OriginalItemCode as 'Scanned Item Code', ItemCode as 'Current Item Code', WarehouseLocation.Name as Location, Status.Name as 'Shipping Status' 
+                From StockItem left join WarehouseLocation on StockItem.Location = WarehouseLocation.Code left join Status on StockItem.Status = Status.Code Where ItemCode = ?", [$i]))->get_results();
+
+                if($new_item_details == null){
+                    $get_errors[] = "Item Code ".$i;
+                }else{
+                    $new_item_details = $new_item_details[0];
+                    $new_items_details[] = $new_item_details;
+                }
             }
-        }       
-        $saved = 'table-warning';
+            $new_items_details_check[$i] = $details;
+        }
     }
 }
 
 if(isset($save) && $save != null){
-    if(is_array($details)){
-        new Query("Update StockItem Set Location = (?) Where OriginalItemCode = ?", [$location_code, $item]);
-    }
-    else{
-        new Query("Update StockItem Set Location = (?) Where ItemCode = ?", [$location_code, $item]);
-    }
+    foreach($new_items_details as $index => $new_item){
+        $item_code = $new_item['Current Item Code'];
+        if(isset($new_items_details_check[$item_code]) && is_array($new_items_details_check[$item_code])){
+            new Query("Update StockItem Set Location = ? Where OriginalItemCode = ?", [$location_code, $item_code]);
+        }
+        else{
+            new Query("Update StockItem Set Location = ? Where ItemCode = ?", [$location_code, $item_code]);
+        }
 
-    $new_item_details['Location'] = strtoupper($location);
-    $saved = 'table-success';
+        $new_items_details[$index]['Location'] = strtoupper($location);
+        $saved = 'table-success';
+    }
 }
 ?>
-
 <br />
 <?php if(isset($get_errors) && $get_errors != null): ?>
     <?php foreach($get_errors as $error): ?>
@@ -74,6 +83,11 @@ if(isset($save) && $save != null){
             There may be an error in your input as <?php echo $error; ?> was not found in the database.
         </div>
     <?php endforeach; ?>
+<?php endif; ?>
+<?php if(isset($saved) && $saved == 'table-success'): ?>
+    <div class="alert alert-success" role="alert">
+        Items have been saved to this location! :)
+    </div>
 <?php endif; ?>
 Number of Rows: <?php echo $get_items->get_row_count(); ?><br />
 <?php if(isset($items) && is_array($items)): ?>
@@ -86,12 +100,14 @@ Number of Rows: <?php echo $get_items->get_row_count(); ?><br />
             </tr>
         </thead>
         <tbody>
-            <?php if(isset($new_item_details) && $new_item_details != null): ?>
-                <tr>
-                    <?php foreach($new_item_details as $new_detail): ?>
-                        <td class="<?php echo $saved; ?>"><?php echo $new_detail; ?></td>
+            <?php if(isset($new_items_details) && $new_items_details != null): ?>
+                    <?php foreach($new_items_details as $new_detail): ?>
+                        <tr>
+                            <?php foreach($new_detail as $new): ?>
+                                <td class="<?php echo $saved; ?>"><?php echo $new; ?></td>
+                            <?php endforeach; ?>
+                        </tr>
                     <?php endforeach; ?>
-                </tr>
             <?php endif; ?>
             <?php foreach($items as $it): ?>
                 <tr>
--- public/theme/body.php
+++ public/theme/body.php
@@ -3,7 +3,7 @@
         // var item_number = document.getElementById("item_group");
         function fetch_location(save) {
             var location = document.getElementById('location').value;
-            var item = document.getElementById('item_number').value;
+            var item = document.getElementById('item_number').value.replace(/\r\n/g,'\n').split('\n');
 
             if (location.length < 3) {
                 document.getElementById("item_group").classList.add('d-none');
@@ -47,14 +47,14 @@
                 </div>
                 <div class="form-group d-none" id="item_group">
                     <label for="item_number">Item Number</label>
-                    <input type="text" oninput="fetch_location()" id="item_number" class="form-control">
+                    <textarea type="text" oninput="fetch_location()" id="item_number" class="form-control" rows="3"></textarea>
                 </div>
                 <!-- <div class="form-group d-none" id="add_item">
                     <button type="button" class="btn btn-outline-primary">Add</button>
                 </div> -->
                 <div class="btn-group d-none" role="group" id="add_item" aria-label="Action" id="add_item">
-                    <button type="button" onclick="fetch_location('save')" class="btn btn-outline-primary">Save item in location</button>
-                    <button type="button" class="btn btn-outline-danger" onclick="document.getElementById('item_number').value=''; fetch_location()">Clear item from location</button>
+                    <button type="button" onclick="fetch_location('save')" class="btn btn-outline-primary">Save items in location</button>
+                    <button type="button" class="btn btn-outline-danger" onclick="document.getElementById('item_number').value=''; fetch_location()">Clear items from location</button>
                 </div>
             </form>
         </div>
