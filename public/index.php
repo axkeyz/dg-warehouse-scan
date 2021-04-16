d1f94bd2527e5ef9d35d6a5479b912671068e763
axkeyz <72898469+axkeyz@users.noreply.github.com>
2021-04-16 21:57:05 +1200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,23 +1,31 @@
 <?php
 
+// Apply main config constants
 include('../config.php');
 
+// Enable debugging system if enabled
 if( APP_DEBUG ){
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
 }
 
+// May need to write an autoloader for classes, but this will do for now
 include('../classes/Query.php');
 
+// ROUTER
+// Get request URI, which is / if hosted in the root or /your-project-folder/ if hosted in project folder
 $request = $_SERVER['REQUEST_URI'];
 
+// Get header for every file. Can use include('theme/header.php') if need php processing.
 readfile('theme/header.html');
 
+// Set out routes
 if($request == '/'.APP_FOLDER.'/'){
     include('theme/functions/warehouse.php');
 }else{
     include('theme/404.php');
 }
 
+// Get footer for every file. Can use include('theme/footer.php') if need php processing.
 readfile('theme/footer.html');
